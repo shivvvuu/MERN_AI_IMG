6f75480313bf67707a6fb5bd9873c96b1ebd68f0
shivam <jhodshivam5122@gmail.com>
2023-01-22 20:18:23 +0530

    backend start1

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,22 @@
+import  express  from "express";
+import * as dotenv from 'dotenv';
+import {Configuration , OpenAIApi} from 'openai' ; 
+
+
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+
+})
+
+const openai = new OpenAIApi(configuration); 
+
+router.route('/').get((req, res) => {
+    res.send("HELLO FROM DALL-E!")
+})
+
+export default router;
